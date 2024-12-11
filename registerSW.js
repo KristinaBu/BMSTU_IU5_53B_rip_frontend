7acced3b0e6843c33df93621e1a2c25490121cb2
KristinaBu <krisdinabui@gmail.com>
2024-12-11 14:40:12 +0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/BMSTU_IU5_53B_rip_frontend/sw.js', { scope: '/BMSTU_IU5_53B_rip_frontend/' })})}
