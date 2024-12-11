f0f89b763f692e96673e4c5fa3b5d5e7d40b942e
KristinaBu <krisdinabui@gmail.com>
2024-12-11 14:37:14 +0300

    lab6 pwa

--- delivery-app/vite.config.ts
+++ delivery-app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,6 +25,32 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Delivery App',
+        short_name: 'Delivery App',
+        description: 'Delivery App',
+        theme_color: '#fdfdfd',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ]
+      }
+
+    })
+
+  ],
   base: "/BMSTU_IU5_53B_rip_frontend/",
 })
